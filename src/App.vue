270a760690a48e2040944c5d0ccfbe3294d2a57d
wangchenxi01 <770036239@qq.com>
2020-12-10 00:25:44 +0800

    first commit vuepro

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script type="text/javascript" src="./js/sakura.js"></script>
+<template>
+  <div id="app" style="width: 100%;height: 100%;">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <img alt="pic1" src="./assets/pic1.jpg" width="50%">
+    <HelloWorld msg="Welcome to my first vue App"/>
+    <h1  style="color:blue">comming soon</h1>
+    <footer><div style="text-align: right;">ver 1.0</div></footer>
+  </div>
+</template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
